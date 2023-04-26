434a2adfbd1462e068314e69cf8189592cc02882
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-26 19:09:04 -0300

    ref: tag nav adicionada

--- src/components/Navegacao.js
+++ src/components/Navegacao.js
@@ -5,15 +5,17 @@ import { House, User, ShoppingCartSimple } from "@phosphor-icons/react";
 export default function Navegacao({ index = 0 }) {
   return (
     <StyledNavegacao index={index}>
-      <Link to="/home">
-        <House size={32} weight="fill" />
-      </Link>
-      <Link to="/user">
-        <User size={32} weight="fill" />
-      </Link>
-      <Link to="/carrinho">
-        <ShoppingCartSimple size={32} weight="fill" />
-      </Link>
+      <nav>
+        <Link to="/home">
+          <House size={32} weight="fill" />
+        </Link>
+        <Link to="/user">
+          <User size={32} weight="fill" />
+        </Link>
+        <Link to="/carrinho">
+          <ShoppingCartSimple size={32} weight="fill" />
+        </Link>
+      </nav>
     </StyledNavegacao>
   );
 }
@@ -26,27 +28,31 @@ const StyledNavegacao = styled.footer`
   height: 74px;
   background: #1e1e1e;
   border-top: 1px solid #2e2e2e;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   padding: 0 32px;
 
-  a {
-    color: rgba(250, 250, 250, 0.4);
-    transition: transform ease 0.25s;
-    &:hover {
-      transform: scale(1.2);
-    }
-    &:active {
-      transform: scale(1);
-    }
-  }
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
 
-  ${({ index }) => {
-    return `
+    a {
+      color: rgba(250, 250, 250, 0.4);
+      transition: transform ease 0.25s;
+      &:hover {
+        transform: scale(1.2);
+      }
+      &:active {
+        transform: scale(1);
+      }
+    }
+
+    ${({ index }) => {
+      return `
         a:nth-of-type(${index + 1}) {
             color: #a3e635;
         }
     `;
-  }};
+    }};
+  }
 `;
