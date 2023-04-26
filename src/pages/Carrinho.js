551fb2a3411948527ca7f32cab486ffd41f9c43d
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-26 19:48:54 -0300

    feat: item do carrinho

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -1,7 +1,32 @@
+import styled from "styled-components";
+import Navegacao from "../components/Navegacao.js";
+import CarrinhoItem from "../components/CarrinhoItem.js";
+
 export default function Carrinho() {
   return (
-    <>
-      <h1>Carrinho de compras</h1>
-    </>
+    <div>
+      <CarrinhoMain>
+        <ul>
+          <li>
+            <CarrinhoItem
+              imagem="https://60398.cdn.simplo7.net/static/60398/sku/masculino-tenis-qix-smash-1640638551971.jpg"
+              titulo="Adidas Shoe"
+              preco={4000}
+            />
+          </li>
+        </ul>
+      </CarrinhoMain>
+      <Navegacao index={2} />
+    </div>
   );
 }
+
+const CarrinhoMain = styled.main`
+  padding: 32px;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+`;
