68056e411892953e907537cd7dc97d154530fd3e
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-26 18:25:14 -0300

    fix: global agora está após o reset

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import GlobalStyle from "./styles/GlobalStyle.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
