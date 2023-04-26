a9b33815b78b85e0a9f822f8501f4cd728bb844a
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-26 19:48:26 -0300

    fix: navbar funcionando com links

---
+++ src/components/CarrinhoItem.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { converterValorParaReais } from "../utils/converterValorParaReais.js";
+
+export default function CarrinhoItem({ imagem, titulo, preco }) {
+  return (
+    <StyledCarrinhoItem>
+      <img src={imagem} alt={titulo} />
+      <StyledCarrinhoItemInformacoes>
+        <div>
+          <p>{titulo}</p>
+          <p>{converterValorParaReais(preco)}</p>
+        </div>
+        <div>
+          <button>+</button>1<button>-</button>
+        </div>
+      </StyledCarrinhoItemInformacoes>
+    </StyledCarrinhoItem>
+  );
+}
+
+const StyledCarrinhoItem = styled.div`
+  background: #404040;
+  height: 118px;
+  width: 100%;
+  border-radius: 12px;
+  overflow: hidden;
+  display: flex;
+
+  p {
+    font-size: 16px;
+  }
+
+  img {
+    height: 100%;
+    width: 136px;
+    object-fit: cover;
+  }
+`;
+
+const StyledCarrinhoItemInformacoes = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  div:first-child {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    text-align: center;
+  }
+
+  div:last-child {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    button {
+      width: 24px;
+      height: 24px;
+      background: #fafafa;
+      border: none;
+      border-radius: 100%;
+      font-size: 14px;
+      font-weight: bold;
+      color: #171717;
+      cursor: pointer;
+
+      transition: transform ease 0.25s;
+      will-change: auto;
+      &:active {
+        transform: scale(0.9);
+      }
+    }
+  }
+`;
