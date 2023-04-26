bf08407d86d1db90c2ddaffffead940254701b8c
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-26 18:23:53 -0300

    style(global): box-sizing, fonte cor de fundo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ResetStyle from "./styles/ResetStyle";
+import GlobalStyle from "./styles/GlobalStyle.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <ResetStyle />
     <App />
   </React.StrictMode>
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		background: #171717;
+		font-family: "Lexend Deca", sans-serif;
+		color: #fafafa;
+	}
+`;
+
+export default GlobalStyle;
